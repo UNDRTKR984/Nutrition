02b9c06c78a60153800f6c82f5423c65d8e0649e
Matt Vogel <mvogel@transy.edu>
2020-06-22 23:59:02 -0400

    Updates

---
+++ precache-manifest.7d6670a959af3ad3e31235af6bdfd0b3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f685eb29cf6df2a6f6d8702e2995efe7",
+    "url": "/Nutrition/index.html"
+  },
+  {
+    "revision": "fb5093b94b39cfb54b66",
+    "url": "/Nutrition/static/css/main.53121036.chunk.css"
+  },
+  {
+    "revision": "8db60a9cc7a7ea534c0d",
+    "url": "/Nutrition/static/js/2.2e7602a4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Nutrition/static/js/2.2e7602a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb5093b94b39cfb54b66",
+    "url": "/Nutrition/static/js/main.4e1477c9.chunk.js"
+  },
+  {
+    "revision": "4ed060fd06ad107a66d9",
+    "url": "/Nutrition/static/js/runtime-main.e95aabda.js"
+  },
+  {
+    "revision": "0be07412a1c196e7cf302e54d02fea48",
+    "url": "/Nutrition/static/media/Nutritionix.0be07412.png"
+  }
+]);
